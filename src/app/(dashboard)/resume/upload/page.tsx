09bcf6739b9da78e1d8bbc7194096f16dbf80e41
Jinsu <injnamek@gmail.com>
2025-07-04 15:30:57 +0900

    ✨ Feat: 미들웨어로 앱 진입시 로그인 분기처리

---
+++ src/app/(dashboard)/resume/upload/page.tsx
@@ -0,0 +1,5 @@
+import ResumeUploadView from "@/sections/view/resume-upload-view";
+
+export default function ResumeUpload() {
+  return <ResumeUploadView />;
+}
---
+++ src/sections/view/resume-upload-view.tsx
@@ -0,0 +1,3 @@
+export default function ResumeUploadView() {
+  return null;
+}
