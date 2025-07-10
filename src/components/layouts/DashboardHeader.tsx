79e3867f69f3e872ea84a3e4ce842b96220a0c96
Jinsu <injnamek@gmail.com>
2025-07-10 14:25:42 +0900

    💄 Style: 헤더 퍼블리싱

--- src/components/layouts/DashboardHeader.tsx
+++ src/components/layouts/DashboardHeader.tsx
@@ -1,36 +1,37 @@
+import { SvgColor } from "@/components/svg-color";
 import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 import { paths } from "@/routes/paths";
 import Link from "next/link";
 
 export default function DashboardHeader() {
   return (
-    <header className="flex items-center justify-between h-20 w-full px-40 bg-yellow-100">
+    <header className="flex items-center justify-between h-20 w-full px-40 border-b border-gray-300">
       {/* left section */}
       <section className="flex gap-[70px]">
-        <div className="size-[42px] bg-[#FB923C]"></div>
+        <SvgColor
+          src="/logo/logo_likelion_primary_24_ver3_color.svg"
+          width={234}
+          className="text-primary-500"
+        />
         <NavigationMenu>
-          <NavigationMenuList className="flex gap-8 *:cursor-pointer">
+          <NavigationMenuList className="flex gap-8 *:cursor-pointer font-semibold">
             <NavigationMenuItem>
-              <NavigationMenuLink>Link</NavigationMenuLink>
+              <NavigationMenuLink>홈</NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink>Link</NavigationMenuLink>
+              <NavigationMenuLink>적성검사</NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink>Link</NavigationMenuLink>
+              <NavigationMenuLink>이력서분석</NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink>Link</NavigationMenuLink>
+              <NavigationMenuLink>마이페이지</NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
