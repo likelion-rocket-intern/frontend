28710d10570fcdcb4d02b8fbe55652d4d855b823
JAWSP <lightload2912@gmail.com>
2025-07-24 16:39:17 +0900

    메인 화면 추가

--- src/components/SplitCard.tsx
+++ src/components/SplitCard.tsx
@@ -6,12 +6,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import React from "react";
+import Image from "next/image";
 
 interface SplitCardProps {
   cardTitle: string;
   cardDescription: React.ReactNode;
   cardActionText: string;
   cardAction: () => void;
+  cardIconSrc: string; // Optional icon source
 }
 
 export default function SplitCard({
@@ -19,16 +21,33 @@ export default function SplitCard({
   cardDescription,
   cardActionText,
   cardAction,
+  cardIconSrc,
 }: SplitCardProps) {
   return (
-    <Card className="flex flex-col justify-end w-[588px] h-[632px] px-[78px] py-[60px] bg-[#D9D9D9]">
-      <CardHeader className="w-[340px] mb-[42px]">
-        <CardTitle className="text-[40px] font-semibold leading-[1.4] tracking-[-1px] mb-5">
+    <Card className="relative flex flex-col items-center justify-end w-[588px] h-[632px] px-[78px] py-[60px] bg-white border border-orange-500 shadow-lg overflow-hidden">
+      {/* 아이콘 아래쪽 사각형 배경 */}
+      <div className="absolute bottom-0 left-0 w-full h-[70%] bg-gradient-to-t from-[#FFF8F3] via-[#FFF8F3] to-transparent opacity-95"></div>
+
+      {cardIconSrc && (
+        <div className="relative mb-12 z-10">
+          <Image src={cardIconSrc} alt={cardTitle} width={240} height={240} />
+        </div>
+      )}
+
+      <CardHeader className="relative z-10 w-[340px] mb-[42px]">
+        <CardTitle className="text-[40px] font-semibold leading-[1.4] tracking-[-1px] mb-5 text-[#505050]">
           {cardTitle}
         </CardTitle>
-        <CardDescription className="body_1">{cardDescription}</CardDescription>
+        <CardDescription className="body_1 text-[#767676]">
+          {cardDescription}
+        </CardDescription>
       </CardHeader>
-      <Button variant={"default_primary"} size={"large"} onClick={cardAction}>
+      <Button
+        variant={"default_primary"}
+        size={"large"}
+        onClick={cardAction}
+        className="relative z-10"
+      >
         {cardActionText}
       </Button>
     </Card>
