baf057632fbd19037fd277e3e84cc5870cd53a7d
Jinsu <injnamek@gmail.com>
2025-07-28 20:34:05 +0900

    🐛 Fix: 미들웨어 리다이렉션 로직을 임시 비활성화하여 무한 리프레시 문제 해결

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,10 +15,10 @@ export function middleware(request: NextRequest) {
   }
 
   // 로그인 상태에서 로그인 페이지에 접근하려고 할 때
-  if (isLoggedIn && isAuthPage) {
-    // 메인 페이지로 리디렉션합니다.
-    return NextResponse.redirect(new URL(paths.root, request.url));
-  }
+  // if (isLoggedIn && isAuthPage) {
+  //   // 메인 페이지로 리디렉션합니다.
+  //   return NextResponse.redirect(new URL(paths.root, request.url));
+  // }
 
   return NextResponse.next();
 }
@@ -26,6 +26,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // API, Next.js 내부 파일, 정적 파일을 제외한 모든 경로에서 미들웨어를 실행합니다.
-    "/((?!api|_next|favicon.ico|.*\.(?:png|jpg|jpeg|svg|webp|ico)).*)",
+    "/((?!api|_next|favicon.ico|.*.(?:png|jpg|jpeg|svg|webp|ico)).*)",
   ],
 };
